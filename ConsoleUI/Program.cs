2be421fb5d8fc231cb1bb71bbfb6732ff874c1f5
acidrod <alarcon.rodrigo@gmail.com>
2019-01-12 21:48:12 -0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Helper;
+
+namespace ConsoleUI
+{
+    class Point
+    {
+        public int xCoord { get; set; }
+        public int yCoord { get; set; }
+
+        public Point(int x, int y)
+        {
+            xCoord = x;
+            yCoord = y;
+        }
+
+        public double Distance(Point q)
+        {
+            double x1, x2, y1, y2;
+            x1 = xCoord;
+            x2 = q.xCoord;
+            y1 = yCoord;
+            y2 = q.yCoord;
+
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Allí cesó tu sed, ¿Oyes eso, Jair amada? ¿Has oído a la odiosa hada María José? Sé yo de su tosecilla";
+            Console.WriteLine(phrase.IsPalindrome());
+
+            int iPalin = 12321;
+            Console.WriteLine(iPalin.IsPalindrome());
+
+            double dPalin = 12.321;
+            Console.WriteLine(dPalin.IsPalindrome());
+
+            Console.WriteLine("Maps, DNA, and spam".IsPalindrome());
+        }
+    }
+}
---
+++ Helper/Palindrome.cs
@@ -0,0 +1,98 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Helper
+{
+    public static class Palindrome
+    {
+        /// <summary>
+        /// Analiza si una frase, palabra o numero son palíndromos.
+        /// </summary>
+        /// <param name="word">Un string para ser analizado</param>
+        /// <returns>Boolean</returns>
+        /// <example>
+        /// //Example 1
+        /// string myPhrase = "Maps, DNA, and spam";
+        /// bool palCheck = myPhrase.IsPalindrome();
+        /// 
+        /// //Example 2
+        /// if("Maps, DNA, and spam".IsPalindrome() == true){
+        ///     Console.WriteLine("You had found a palindrome!, congratulations!")
+        /// }
+        /// else
+        /// {
+        ///     Console.WriteLine("Nope!, try a new phrase...");
+        /// }
+        /// </example>
+
+        // Extention Method for strings
+        public static bool IsPalindrome(this string word)
+        {
+            //Normaliza la frase para analizar
+            word = FixWord(word);
+
+            //Valida que word tenga al menos 3 caracteres de largo para analizar
+            if (!(word.Length >= 3))
+            {
+                return false;
+            }
+
+            int posIzq = 0;
+            int posDer = word.Length-1;
+            
+            //Recorre la variable word desde izquierda a derecha letra por letra
+            for (int i = 0; i < word.Length-1; i++)
+            {
+                if (word[posIzq] == word[posDer])
+                {
+                    posIzq++;
+                    posDer--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Extention Method for integers
+        public static bool IsPalindrome(this int number)
+        {
+            return IsPalindrome(number.ToString());
+        }
+
+        // Extention Method for doubles
+        public static bool IsPalindrome(this double number)
+        {
+            return IsPalindrome(number.ToString());
+        }
+
+        private static string FixWord(string word)
+        {
+            //Initial normalization (Trim, LowerCase, Replace (blanks, colons and periods)
+            word = word.Trim().ToLower().Replace(" ", "").Replace(".", "").Replace(",", "");
+
+            //Remove Specials Pass 1
+            Regex pattern = new Regex("[:!¡“”@#$%^&*()}{|\":¿?><\\[\\]\\;'/.,~]");
+            word = pattern.Replace(word,"");
+
+            //Remove Specials Pass 2 (Language special characters)
+            string normalized = word.Normalize(NormalizationForm.FormD);
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var c in normalized)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    builder.Append(c);
+                }
+            }
+
+            return builder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
